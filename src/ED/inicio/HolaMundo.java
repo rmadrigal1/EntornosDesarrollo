bc73c1d4a10d6d2da86856d8412c521f90bf21b2
rmadrigal <rafamtv@gmail.com>
2022-11-22 10:19:31 +0100

    Se añade variable y su impresión

--- src/ED/inicio/HolaMundo.java
+++ src/ED/inicio/HolaMundo.java
@@ -5,6 +5,11 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!!");
 		System.out.println("Hola Mundo de nuevo!!!");
+		
+		int numero = 0;
+		
+		System.out.println(numero);
+		
 	}
 
 }
