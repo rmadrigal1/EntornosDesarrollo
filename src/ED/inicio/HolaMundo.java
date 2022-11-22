6af7b0e6dc381053ad301887310b3a217a804692
rmadrigal <rafamtv@gmail.com>
2022-11-22 10:15:20 +0100

    En hola mundo se ponen 3 líneas

--- src/ED/inicio/HolaMundo.java
+++ src/ED/inicio/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!!");
+		System.out.println("Hola Mundo de nuevo!!!");
+		System.out.println("Hacemos cambios");
 	}
 
 }
