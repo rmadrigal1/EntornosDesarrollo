b2dc9150dacd89953e94fe4622e76ca762515f22
rmadrigal <rafamtv@gmail.com>
2022-11-22 10:17:02 +0100

    Se elimina una línea.Se quedan 2.

--- src/ED/inicio/HolaMundo.java
+++ src/ED/inicio/HolaMundo.java
@@ -5,7 +5,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!!");
 		System.out.println("Hola Mundo de nuevo!!!");
-		System.out.println("Hacemos cambios");
 	}
 
 }
