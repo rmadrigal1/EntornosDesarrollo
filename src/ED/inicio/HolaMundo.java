43bd842c04f373378f80c6b86998d6172f1b8dfd
rmadrigal <rafamtv@gmail.com>
2022-11-22 10:37:05 +0100

    Se deja en 5 líneas

--- src/ED/inicio/HolaMundo.java
+++ src/ED/inicio/HolaMundo.java
@@ -3,7 +3,14 @@ package ED.inicio;
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		System.out.println("COMIENZA EL PROYECTO");
 		System.out.println("Hola Mundo!!!");
+		
+		int numero=0;
+		
+		System.out.println(numero);
+		System.out.println("FIN DEL PROYECTO");
+		
 	}
 
 }
